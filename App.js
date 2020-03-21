43c20445bbf835a2c625bb39f613870441fcef25
daltonlcarr <daltcarr@gmail.com>
2020-03-21 14:27:14 -0700

    mvps done except for limit one per day, basic styles added using amplify ui guideline styles

--- App.js
+++ App.js
@@ -5,100 +5,116 @@ import * as Font from 'expo-font';
 import { Ionicons } from '@expo/vector-icons';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-Amplify.configure(config)
-import Amplify, { API, graphqlOperation } from "aws-amplify"
-import config from "./aws-exports"
+Amplify.configure(config);
+import Amplify, { API, graphqlOperation } from 'aws-amplify';
+import config from './aws-exports';
 import BottomTabNavigator from './navigation/BottomTabNavigator';
 import useLinking from './navigation/useLinking';
 import { withAuthenticator } from 'aws-amplify-react-native';
-import {CustomAuthTheme} from './components/CustomAuthTheme'
-
+import { CustomAuthTheme } from './components/CustomAuthTheme';
 
 // import './styles/App.scss';
 
 const Stack = createStackNavigator();
 
 function App(props) {
-  const [isLoadingComplete, setLoadingComplete] = React.useState(false);
-  const [initialNavigationState, setInitialNavigationState] = React.useState();
-  const containerRef = React.useRef();
-  const { getInitialState } = useLinking(containerRef);
+	const [ isLoadingComplete, setLoadingComplete ] = React.useState(false);
+	const [ initialNavigationState, setInitialNavigationState ] = React.useState();
+	const containerRef = React.useRef();
+	const { getInitialState } = useLinking(containerRef);
 
-  // Load any resources or data that we need prior to rendering the app
-  React.useEffect(() => {
-    async function loadResourcesAndDataAsync() {
-      try {
-        SplashScreen.preventAutoHide();
+	// Load any resources or data that we need prior to rendering the app
+	React.useEffect(() => {
+		async function loadResourcesAndDataAsync() {
+			try {
+				SplashScreen.preventAutoHide();
 
-        // Load our initial navigation state
-        setInitialNavigationState(await getInitialState());
+				// Load our initial navigation state
+				setInitialNavigationState(await getInitialState());
 
-        // Load fonts
-        await Font.loadAsync({
-          ...Ionicons.font,
-          'space-mono': require('./assets/fonts/SpaceMono-Regular.ttf'),
-        });
-      } catch (e) {
-        // We might want to provide this error information to an error reporting service
-        console.warn(e);
-      } finally {
-        setLoadingComplete(true);
-        SplashScreen.hide();
-      }
-    }
+				// Load fonts
+				await Font.loadAsync({
+					...Ionicons.font,
+					'space-mono': require('./assets/fonts/SpaceMono-Regular.ttf')
+				});
+			} catch (e) {
+				// We might want to provide this error information to an error reporting service
+				console.warn(e);
+			} finally {
+				setLoadingComplete(true);
+				SplashScreen.hide();
+			}
+		}
 
-    loadResourcesAndDataAsync();
-  }, []);
+		loadResourcesAndDataAsync();
+	}, []);
 
-  if (!isLoadingComplete && !props.skipLoadingScreen) {
-    return null;
-  } else {
-    return (
-      <View style={styles.container}>
-        {Platform.OS === 'ios' && <StatusBar barStyle="default" />}
-        <NavigationContainer ref={containerRef} initialState={initialNavigationState}>
-          <Stack.Navigator>
-            <Stack.Screen name="Root" component={BottomTabNavigator} />
-          </Stack.Navigator>
-        </NavigationContainer>
-        
-      </View>
-    );
-  }
+	if (!isLoadingComplete && !props.skipLoadingScreen) {
+		return null;
+	} else {
+		return (
+			<View style={styles.container}>
+				{Platform.OS === 'ios' && <StatusBar barStyle="default" />}
+				<NavigationContainer ref={containerRef} initialState={initialNavigationState}>
+					<Stack.Navigator>
+						<Stack.Screen
+							options={{
+								title: 'My home',
+								headerStyle: {
+									backgroundColor: '#152939'
+								},
+								headerTintColor: '#fff',
+								headerTitleStyle: {
+                  fontWeight: 'bold',
+                  fontFamily: 'Chalkboard SE',
+                  fontSize: 24
+								}
+							}}
+							name="Root"
+							component={BottomTabNavigator}
+						/>
+					</Stack.Navigator>
+				</NavigationContainer>
+			</View>
+		);
+	}
 }
 
 const signUpConfig = {
-  header: 'Sign Up',
-  hideAllDefaults: true,
-  hideSignUp: true,
-  signUpFields: [
-    {
-      label: 'Email',
-      key: 'email',
-      required: true,
-      displayOrder: 1,
-      type: 'email'
-    },
-    {
-      label: 'Password',
-      key: 'password',
-      required: true,
-      displayOrder: 2,
-      type: 'password'
-    }
-  ]
+	header: 'Sign Up',
+	hideAllDefaults: true,
+	hideSignUp: true,
+	signUpFields: [
+		{
+			label: 'Email',
+			key: 'email',
+			required: true,
+			displayOrder: 1,
+			type: 'email'
+		},
+		{
+			label: 'Password',
+			key: 'password',
+			required: true,
+			displayOrder: 2,
+			type: 'password'
+		}
+	]
 };
 
 export default withAuthenticator(App, {
-  // Render a sign out button once logged in
-  includeGreetings: true, 
+	// Render a sign out button once logged in
+	// includeGreetings: true,
   signUpConfig,
+  theme: {
+    button: { backgroundColor: '#6ec2d7', fontWeight: 'bold', borderRadius: '4px', margin: '0 auto' },
+    a: { color: '#6ec2d7'}
+  }
 });
 
-
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-  }
+	container: {
+		flex: 1,
+		backgroundColor: '#e6e6e6'
+	}
 });
--- screens/SearchScreen.js
+++ screens/SearchScreen.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import { StyleSheet, Text, View, TextInput, TouchableOpacity, ScrollView, Linking, Platform } from 'react-native';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity, ScrollView, KeyboardAvoidingView } from 'react-native';
 import { listTodos } from '../src/graphql/queries';
 import { createTodo } from '../src/graphql/mutations';
 import { API, graphqlOperation } from 'aws-amplify';
+
 
 export default class App extends React.Component {
 	state = {
@@ -47,26 +48,12 @@ export default class App extends React.Component {
 		this.setState({ todos: updatedTodo, name: '', zip: '', phoneNumber: '', showInputs: false });
 	};
 
-	dialCall = (number) => {
- 
-		let phoneNumber = '';
-	 
-		if (Platform.OS === 'android') {
-		  phoneNumber = number;
-		}
-		else {
-		  phoneNumber = number;
-		}
-	 
-		Linking.openURL(phoneNumber);
-	  };
-
 	render() {
 		return (
 			<View style={styles.container}>
 				{this.state.showInputs === false && (
 					<ScrollView style={styles.scrollView}>
-						{this.state.todos.map((todo, index) => (
+						{this.state.todos.filter((todo) => todo.zip == this.state.ZIPfilter).map((todo, index) => (
 							<View key={index} style={styles.todo}>
 								<Text style={styles.nameTitle}>
 									Items: <Text style={styles.name}>{todo.name}</Text>
@@ -74,64 +61,31 @@ export default class App extends React.Component {
 								<Text style={styles.nameTitle}>
 									ZIP: <Text style={styles.name}>{todo.zip}</Text>
 								</Text>
-								<Text style={styles.nameTitle} onPress={this.dialCall(todo.phoneNumber)} >
-									Phone or WhatsApp: <Text style={styles.name}>{todo.phoneNumber}</Text>
+								<Text style={styles.nameTitle}>
+									Phone: <Text style={styles.name}>{todo.phoneNumber}</Text>
 								</Text>
 							</View>
 						))}
 					</ScrollView>
 				)}
-				{this.state.showInputs && (
-					<View>
-						<TextInput
-							style={styles.input}
-							value={this.state.name}
-							onChangeText={(val) => this.onChangeText('name', val)}
-							placeholder="Items needed, seperated by commas. Ex: bread, eggs, water)"
-							maxLength={100}
-						/>
-						<TextInput
-							style={styles.input}
-							value={this.state.zip}
-							onChangeText={(val) => this.onChangeText('zip', val)}
-							placeholder="Enter ZIP Code"
-							maxLength={5}
-							// minLength={5}
-							keyboardType={'numeric'}
-						/>
-						<TextInput
-							style={styles.input}
-							value={this.state.phoneNumber}
-							onChangeText={(val) => this.onChangeText('phoneNumber', val)}
-							placeholder="Phone number"
-							maxLength={12}
-							// minLength={12}
-							keyboardType={'numeric'}
-
-						/>
-						<TouchableOpacity onPress={this.addInfo} style={styles.buttonContainer}>
-							<Text style={styles.buttonText}>Add</Text>
-						</TouchableOpacity>
-						<TouchableOpacity
-							onPress={() => this.setState({ showInputs: false })}
-							style={styles.buttonContainer}
-						>
-							<Text style={styles.buttonText}>Back</Text>
-						</TouchableOpacity>
-					</View>
-				)}
 				{this.state.showInputs === false && (
-					<View style={styles.filterAndAdd}>
-						
-						<TouchableOpacity
-							onPress={() => this.setState({ showInputs: true })}
-							style={styles.addbuttonContainer}
-						>
-							<Text style={styles.buttonText}>Add + </Text>
-						</TouchableOpacity>
-
-					</View>
+					<KeyboardAvoidingView
+                        style={styles.container}
+                        behavior="padding"
+                    >
+						<View style={styles.filterAndAdd}>
+							<TextInput
+								style={styles.zipFilterInput}
+								value={this.state.ZIPfilter}
+								onChangeText={(val) => this.onChangeText('ZIPfilter', val)}
+								placeholder="Enter ZIP to find people near you"
+								maxLength={5}
+								keyboardType={'numeric'}
+							/>
+						</View>
+					</KeyboardAvoidingView>
 				)}
+
 			</View>
 		);
 	}
@@ -140,28 +94,30 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
 	container: {
 		flex: 1,
-		backgroundColor: '#f4f4f4',
+		backgroundColor: '#fff',
 		paddingHorizontal: 10,
 		paddingTop: 20
 	},
 	input: {
 		height: 50,
 		borderBottomWidth: 2,
-		borderBottomColor: 'lightblue',
+		borderBottomColor: '#31465F',
 		marginVertical: 10
 	},
 	zipFilterInput: {
 		height: 50,
 		borderBottomWidth: 2,
-		borderBottomColor: 'lightblue',
+		borderBottomColor: '#31465F',
 		marginVertical: 10,
-		fontSize: 16
+        fontSize: 16,
+        fontFamily: 'Chalkboard SE',
+
 	},
-	filterAndAdd:{
-		marginBottom:20
+	filterAndAdd: {
+		marginBottom: 20
 	},
 	buttonContainer: {
-		backgroundColor: '#6ec2d7',
+		backgroundColor: '#34495e',
 		marginTop: 10,
 		marginBottom: 20,
 		padding: 10,
@@ -169,7 +125,7 @@ const styles = StyleSheet.create({
 		alignItems: 'center'
 	},
 	addbuttonContainer: {
-		backgroundColor: '#FFAC31',
+		backgroundColor: '#34495e',
 		marginTop: 10,
 		padding: 10,
 		borderRadius: 5,
@@ -177,8 +133,7 @@ const styles = StyleSheet.create({
 	},
 	buttonText: {
 		color: '#fff',
-		fontSize: 24,
-		fontFamily: 'Chalkboard SE',
+		fontSize: 24
 	},
 
 	todo: {
@@ -192,31 +147,27 @@ const styles = StyleSheet.create({
 		backgroundColor: '#31465F'
 	},
 	name: {
-		fontSize: 14,
+		fontSize: 16,
 		fontWeight: 'normal',
 		paddingHorizontal: 10,
-		textTransform: 'uppercase',
-		color: 'white',
-		fontFamily: 'Chalkboard SE',
-
+        textTransform: 'uppercase',
+        fontFamily: 'Chalkboard SE',
+        color: 'white'
 	},
 
 	nameTitle: {
 		fontWeight: 'bold',
-		paddingHorizontal: 10,
-		color: 'white',
-		fontSize: 16,
-		fontFamily: 'Chalkboard SE',
-
-
+        paddingHorizontal: 10,
+        color: 'white',
+        fontFamily: 'Chalkboard SE',
 	},
-	or:{
-		fontSize:20,
+	or: {
+		fontSize: 20,
 		marginVertical: 20,
 		textAlign: 'center'
 	},
 	scrollView: {
-		backgroundColor: '#f4f4f4',
+		backgroundColor: 'white',
 		marginHorizontal: -10
 	}
 });
